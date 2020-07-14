c23a2642249d0cb9aea62ada38c2511ae3347357
kabilangr <g.r.kabilan@gmail.com>
2020-07-14 11:09:40 +0530

    Finished

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,13 +1,20 @@
 package controller;
 
-import java.sql.Connection;
+import java.sql.*;
+
 
 import utility.ConnectionManager;
 
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Exception {
 		
-	// Fill your code
-		
+		Connection c=null;
+		c=ConnectionManager.getConnection();
+		if(c!=null)
+		{
+			System.out.println("Connection Established");
+		}
+		else
+			System.out.println("Check connection");
 	}
 }
